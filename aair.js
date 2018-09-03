6a26c251491d891b75ed4b57a8c204b564494bf3
toshuu <toshuu.ps4@gmail.com>
2018-09-04 01:12:15 +0530

    Create aair.js

---
+++ aair.js
@@ -0,0 +1,9 @@
+var uname= 'Tushar Super';
+var ushop= 'Vio Smart Products';
+var uwano= '917218824193';
+var umail= 'liam.zinc@gmail.com';
+var usubscription= 'Last Paid : 30/04/2018';
+var ustatus= 'Connection Active.';
+var uvideo = '<p>How to jump 4X growth?</p>';
+var biturl = 'http://aair.netlify.com/v/i';
+var apptitle = 'Tushar\'s Catalogs'
