7eec87802064f46d229a3b984f063041259b2a76
toshuu <toshuu.ps4@gmail.com>
2018-09-04 00:13:25 +0530

    Add files via upload

---
+++ gi.js
@@ -0,0 +1,9 @@
+var uname= 'Tushar Thakare';
+var ushop= 'Vio Smart Products';
+var uwano= '917218824193';
+var umail= 'liam.zinc@gmail.com';
+var usubscription= 'Last Paid : 30/04/2018';
+var ustatus= 'Connection Active.';
+var uvideo = '<p>How to jump 4X growth?</p>';
+var biturl = 'http://tushar.io';
+var apptitle = 'Tushar\'s Shop'
