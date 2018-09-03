89b48aab2c73ef4dd367d98c360d2bc6a411785c
toshuu <toshuu.ps4@gmail.com>
2018-09-04 00:18:25 +0530

    Update gi.js

--- gi.js
+++ gi.js
@@ -6,4 +6,4 @@ var usubscription= 'Last Paid : 30/04/2018';
 var ustatus= 'Connection Active.';
 var uvideo = '<p>How to jump 4X growth?</p>';
 var biturl = 'http://tushar.io';
-var apptitle = 'Tushar\'s Shop'
+var apptitle = 'Tushar\'s Shsop'
